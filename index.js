176b488eb92cfeaa256031d22a9741465770866e
Your Name <you@example.com>
2018-07-24 09:17:37 +0000

    v8 version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('This is new deployments');
+  res.send('It is a new deployment for spinnaker automated build');
 });
 
 var server = app.listen(3000, function () {
