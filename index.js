93b6e538b9ff50260d172c266c58aced5684fd8f
Your Name <you@example.com>
2018-07-25 09:21:18 +0000

    v9 commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('It is a new deployment for spinnaker automated build');
+  res.send('It is  a demo app.');
 });
 
 var server = app.listen(3000, function () {
