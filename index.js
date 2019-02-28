63783c58c9eee826c22ec3309a81f65c606c068f
Your Name <you@example.com>
2019-02-28 13:21:41 +0000

    demo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('EMS Spinnaker Kubernetes Demo!! on 28 Feb 2019');
+  res.send('EMS Spinnaker Kubernetes Demo!! v11.8 on 28 Feb 2019');
 });
 
 var server = app.listen(3000, function () {
